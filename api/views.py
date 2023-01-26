91a4a771720e689747fc6e5a637a60450e460905
Eden Brekke <eden.lorrai@gmail.com>
2023-01-26 14:30:27 -0800

    added taskList method

--- api/views.py
+++ api/views.py
@@ -3,6 +3,9 @@ from django.http import JsonResponse
 
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .serializers import TaskSerializer
+
+from .models import Task
 
 # Create your views here.
 
@@ -16,4 +19,10 @@ def apiOverview(request):
         'Delete':'/task-delete/<str:pk>/',
         }
     
-    return Response('API BASE POINT', safe=False)
+    return Response(api_urls)
+  
+@api_view(['GET']) 
+def taskList(request):
+    tasks = Task.objects.all()
+    serializer = TaskSerializer(tasks, many=True)
+    return Response(serializer.data)
