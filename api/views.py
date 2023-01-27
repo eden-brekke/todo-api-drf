860e2e4fe7a8a6e6b5ba78fd07bd9712e957d67a
Eden Brekke <eden.lorrai@gmail.com>
2023-01-27 15:39:45 -0800

    fixed formatting

--- api/views.py
+++ api/views.py
@@ -9,52 +9,58 @@ from .models import Task
 
 # Create your views here.
 
+
 @api_view(['GET'])
 def apiOverview(request):
     api_urls = {
-        'List':'/task-list/',
+        'List': '/task-list/',
         'Detail View': '/task-detail/<str:pk>/',
-        'Create':'/task-create/',
-        'Update':'/task-update/<str:pk>/',
-        'Delete':'/task-delete/<str:pk>/',
-        }
-    
+        'Create': '/task-create/',
+        'Update': '/task-update/<str:pk>/',
+        'Delete': '/task-delete/<str:pk>/',
+    }
+
     return Response(api_urls)
-  
-@api_view(['GET']) 
+
+
+@api_view(['GET'])
 def taskList(request):
     tasks = Task.objects.all()
     serializer = TaskSerializer(tasks, many=True)
     return Response(serializer.data)
-  
-@api_view(['GET']) 
+
+
+@api_view(['GET'])
 def taskDetail(request, pk):
     tasks = Task.objects.get(id=pk)
     serializer = TaskSerializer(tasks, many=False)
     return Response(serializer.data)
-  
-@api_view(['POST']) 
+
+
+@api_view(['POST'])
 def taskCreate(request):
     serializer = TaskSerializer(data=request.data)
-    
+
     if serializer.is_valid():
-      serializer.save()
-    
+        serializer.save()
+
     return Response(serializer.data)
-  
-@api_view(['POST']) 
+
+
+@api_view(['POST'])
 def taskUpdate(request, pk):
     task = Task.objects.get(id=pk)
     serializer = TaskSerializer(instance=task, data=request.data)
-    
+
     if serializer.is_valid():
-      serializer.save()
-    
+        serializer.save()
+
     return Response(serializer.data)
 
-@api_view(['DELETE']) 
+
+@api_view(['DELETE'])
 def taskDelete(request, pk):
     task = Task.objects.get(id=pk)
     task.delete()
-    
+
     return Response('Item successfully deleted')
