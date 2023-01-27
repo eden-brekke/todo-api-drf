36525d536fd34035d30f48436acca70f3cf9d04c
Eden Brekke <eden.lorrai@gmail.com>
2023-01-27 15:38:22 -0800

    added fields to model

--- api/models.py
+++ api/models.py
@@ -3,6 +3,8 @@ from django.db import models
 # Create your models here.
 class Task(models.Model):
   title = models.CharField(max_length=200)
+  description = models.TextField()
+  priority = models.IntegerField(null=True)
   completed = models.BooleanField(default=False, blank=True, null=True)
   
   def __str__(self):
