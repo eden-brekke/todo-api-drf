3eac0c59c7239b98ac51b377aeaff417571bdf67
Eden Brekke <eden.lorrai@gmail.com>
2023-01-28 14:47:26 -0800

    change for troubleshooting

--- api/models.py
+++ api/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Task(models.Model):
   title = models.CharField(max_length=200)
   description = models.TextField()
-  priority = models.IntegerField(null=True)
+  priority = models.IntegerField(default=1)
   completed = models.BooleanField(default=False, blank=True, null=True)
   
   def __str__(self):
